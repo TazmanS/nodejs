e8454897e4e117e01e41926e4949c38c4f2ce81d
tazmans <tazmans123@gmail.com>
2020-12-29 15:46:19 +0200

    block 3 end

--- index.js
+++ index.js
@@ -1,74 +1,32 @@
-const http = require('http')
+const express = require('express')
+const exphbs = require('express-handlebars')
 const path = require('path')
-const fs = require('fs')
+const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
+const addRoutes = require('./routes/add')
+const coursesRoutes = require('./routes/courses')
 
-const server = http.createServer((req, res) => {
-  if (req.method === "GET") {
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    })
+const app = express()
 
-    if(req.url === '/'){
-      fs.readFile(path.join(__dirname, 'views', 'index.html'),
-        'utf-8',
-        (err, content) => {
-          if(err){
-            throw Error
-          }
-
-          res.end(content)
-        }
-      )
-    } else if (req.url === '/about') {
-      fs.readFile(path.join(__dirname, 'views', 'about.html'),
-        'utf-8',
-        (err, content) => {
-          if(err){
-            throw Error
-          }
-
-          res.end(content)
-        }
-      )
-    } else if (req.url === '/api/users') {
-      res.writeHead(200, {
-        'Content-Type': 'text/json'
-      })
-
-      const users = [
-        {
-          name: 'abc',
-          age: 11
-        },
-        {
-          name: 'cba',
-          age: 222
-        }
-      ]
-
-      res.end(JSON.stringify(users))
-    }
-
-  } else if(req.method === "POST") {
-    const body = []
-    res.writeHead(200, {
-      'Content-Type': 'text/html; charset=utf-8'
-    })
-
-    req.on('data', data => {
-      body.push(Buffer.from(data))
-    })
-
-    req.on('end', () => {
-      const message = body.toString().split('=')[1]
-
-      res.end(`
-        <h1>Ваше сообщение: ${message}</h1>
-      `)
-    })
-  }
+const hbs = exphbs.create({
+  defaultLayout: 'main',
+  extname: 'hbs'
 })
 
-server.listen(3000, () => {
-  console.log("Server is running")
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log("Server run")
 })
