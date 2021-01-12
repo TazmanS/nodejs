51abdba5dbb4eac2718e005a98eeee7d346a577e
tazmans <tazmans123@gmail.com>
2021-01-12 13:11:54 +0200

    heroku

--- keys/index.js
+++ keys/index.js
@@ -1,7 +1,5 @@
-module.exports = {
-  MONGODB_URI: 'mongodb+srv://admin:admin123@nodejs.tpw1l.mongodb.net/nodejs',
-  SESSION_SECRET: 'some secret value',
-  SENDGRID_API_KEY: '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',
-  EMAIL_FROM: 'tazmans123@gmail.com',
-  BASE_URL: 'localhost:3000'
+if (process.env.NODE_ENV === 'prodaction') {
+  module.exports = require('./keys.prod')
+} else {
+  module.exports = require('./keys.dev')
 }
